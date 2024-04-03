e036895ce0de500a92b4f78b8d61a1dd9b276796
jmcb <joelsgp@protonmail.com>
2024-04-03 17:11:22 +0100

    Fix script

--- new-db.sh
+++ new-db.sh
@@ -2,7 +2,9 @@
 set -eux
 
 working='archive.db'
+destdir='archive'
 
+mkdir -p "${destdir}"
 timestamp=$(date '+%Y%m%dT%H%M%SZ')
-bsdtar -a -cf "archive/archive-${timestamp}.db.xz" "${working}"
+bsdtar -a -cf "${destdir}/archive-${timestamp}.db.xz" "${working}"
 rm "${working}"
